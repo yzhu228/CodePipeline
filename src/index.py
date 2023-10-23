8ca5ad668a1e6635e3ac641884bfcd4ab8dbe528
Yu Zhu <yu.zhu@purple.telstra.com>
2023-10-23 23:45:34 +1100

    Update py handler

--- src/index.py
+++ src/index.py
@@ -1,2 +1,15 @@
+import os
+import json
+
 def lambda_handler(event, context):
-    "Hello World!"
+   '''Lambda handler'''
+   json_region = os.environ['AWS_REGION']
+   return {
+      "statusCode": 200,
+      "headers": {
+         "Content-Type": "application/json"
+      },
+      "body": json.dumps({
+         "Region": json_region
+      })
+   }
